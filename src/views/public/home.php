793d4ac4121b1d87bfbfa7f792ffa4dc1cca10ff
TequilAle <alekabone@gmail.com>
2024-12-04 20:07:13 -0700

    home reparado
    home reparad

--- src/views/public/home.php
+++ src/views/public/home.php
@@ -1,38 +1,38 @@
 <?php
 
-    require_once "/xampp/htdocs/src/config/database.php";
+require_once "/xampp/htdocs/src/config/database.php";
 
-    $conn = Db::getPDO();
-    $error =  null;
+$conn = Db::getPDO();
+$error = null;
 
-    // Obetner pelicula mas reciente
-    $movies = [];
-    try {
-        $stmt = $conn->query("SELECT * FROM movies");
-        $movies = $stmt->fetchAll(PDO::FETCH_ASSOC);
+// Obetner pelicula mas reciente
+$movies = [];
+try {
+    $stmt = $conn->query("SELECT * FROM movies");
+    $movies = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    } catch (PDOException $e) {
-        $error = 'Algo salio mal al cargar las peliculas';
-    }  
-    
-    // Obtener evento mas reciente
-    $event = null;
-    try {
-        $stmt = $conn->query("SELECT * FROM events ORDER BY id DESC LIMIT 1");
-        $event = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        
-    } catch (PDOException $e) {
-        $error = 'Algo salio mal al cargar el evento';
-    }   
+} catch (PDOException $e) {
+    $error = 'Algo salio mal al cargar las peliculas';
+}
+
+// Obtener evento mas reciente
+$event = null;
+try {
+    $stmt = $conn->query("SELECT * FROM events ORDER BY id DESC LIMIT 1");
+    $event = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+} catch (PDOException $e) {
+    $error = 'Algo salio mal al cargar el evento';
+}
 ?>
 
 <?php include './src/views/public/layouts/header.php'; ?>
 
 <main>
     <?php if (count($movies) == 0): ?>
-            <section class="container-fluid d-flex justify-content-center">
-                <h3 style="max-width: 400px;" class="text-center bg-white p-3 rounded shadow">No hay peliculas en cartelera</h3>
-            </section>
+        <section class="container-fluid d-flex justify-content-center">
+            <h3 style="max-width: 400px;" class="text-center bg-white p-3 rounded shadow">No hay peliculas en cartelera</h3>
+        </section>
     <?php else: ?>
         <div class="content">
             <div class="left-frame">
@@ -40,13 +40,14 @@
                     <h1><b>Hoy</b></h1>
                 </div>
 
-                <?php foreach($movies as $movie): ?>
-                    <div class="frameGrid">
+                <div class="frameGrid">
+                    <?php foreach ($movies as $movie): ?>
+
                         <div class="filmFrame" id="frame0">
                             <div class="innerFrame">
 
                                 <div class="todayPoster">
-                                    <img src="<?= explode('htdocs', $movie['img_route'])[1]?>" alt="poster" class="poster">
+                                    <img src="<?= explode('htdocs', $movie['img_route'])[1] ?>" alt="poster" class="poster">
                                 </div>
 
                                 <div class="description">
@@ -64,20 +65,23 @@
                                 </div>
                             </div>
                         </div>
-                    </div>
-                <?php endforeach; ?>
+                    <?php endforeach; ?>
+
+                </div>
+            </div>
+        <?php endif; ?>
+        <div class="right-frame">
+            <div class="frameTitle">
+                <h1><b>Proximo evento</b></h1>
+            </div>
+
+            <div class="eventContainer">
+                <div class="eventPoster">
+                    <img src="<?= explode('htdocs', $events['img_route'])[1] ?>" alt="Evento ejemplo" >
+                </div>
+            </div>
+
         </div>
-    <?php endif; ?>
-    <div class="right-frame">
-                        <div class="frameTitle">
-                            <h1><b>Proximo evento</b></h1>
-                        </div>
-
-                        <div class="eventContainer">
-                            <img src="<?= explode('htdocs', $events['img_route'])[1]?>" alt="Evento ejemplo" class="eventPoster">
-                        </div>
-
-                    </div>
 </main>
 
 <?php include './src/views/public/layouts/footer.php'; ?>
