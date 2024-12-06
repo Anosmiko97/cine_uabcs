7d8cc7dfa94d9f3c17f31a7e41fdd1d96026e2f1
UrieL0x <143738012+UriL0x@users.noreply.github.com>
2024-12-05 21:51:20 -0700

    footer arreglado

--- src/views/public/layouts/footer.php
+++ src/views/public/layouts/footer.php
@@ -1,36 +1,58 @@
 <footer class="text-white py-4 mt-auto" style="background-color: #2D6FA4;">
         <div class="container">
-            <div class="row">
-                <!-- Columna 1: Información general -->
-                <div class="col-md-4 mb-3">
-                    <h5>Universidad Autonoma de Baja California Sur</h5>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultricies lacinia sapien.</p>
-                </div>
-                <!-- Columna 2: Enlaces -->
-                <div class="col-md-4 mb-3">
-                    <h5>Enlaces rápidos</h5>
+            <div class="row text-center">
+
+                <div class="col-md-4 mb-3 text-start">
+                    <h5 class="fw-bold">Enlaces de interes</h5>
                     <ul class="nav flex-column">
                         <li class="nav-item">
-                            <a class="nav-link text-white" href="#">Inicio</a>
+                            <a class="nav-link text-white" href="">Consultorio social</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link text-white" href="#">Acerca de</a>
+                            <a class="nav-link text-white" href="">Oferta educativa</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link text-white" href="#">Contacto</a>
+                            <a class="nav-link text-white" href="">Responsabilidad Social universitaria</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="">Transparencia</a>
                         </li>
                     </ul>
                 </div>
-                <!-- Columna 3: Redes sociales -->
-                <div class="col-md-4 mb-3">
-                    <h5>Síguenos</h5>
-                    <div class="d-flex flex-wrap">
-                        <a href="#" class="text-white me-3 mb-2"><i class="bi bi-facebook"></i> Facebook</a>
-                    </div>
+
+                <div class="col-md-4 mb-3 text-start">
+                    <h5 class="fw-bold">Nuestra Universidad</h5>
+                    <ul class="nav flex-column">
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="">Calendario escolar</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="">Curriculum institucional</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="">Mapa del Campus</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="">Normalidad</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="">Organos de gobierno</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="col-md-4 mb-3 text-start">
+                    
+                        <img src="/src/views/public/assets/media/movies/default/uabcsLogo.png" alt="Logo" class="me-2" style="max-width: 250px; height: auto;">
+                    
+                    <p class="mt-4">
+                    Boulevard Forjadores S/N entre Calle Av. Universidad y Calle Félix Agramont Cota Col. Universitario. La Paz, Baja California Sur, México
+                    Tel: 612-123-8800
+                    </p>
                 </div>
             </div>
-            <div class="text-center mt-4">
-                <p class="mb-0">&copy; 2024 UABCS Cine.</p>
+            <div class="text-center mt-4 border-top">
+                <p class="mt-4">&copy; 2024 UABCS Cine.</p>
             </div>
         </div>
     </footer>
